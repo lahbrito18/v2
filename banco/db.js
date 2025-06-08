5d2b7d7d8b61e932ef325ba30e446ae357ba9859
lahbrito18 <134542731+lahbrito18@users.noreply.github.com>
2025-06-08 12:45:48 -0400

    Update db.js

--- banco/db.js
+++ banco/db.js
@@ -1,11 +1,9 @@
-// banco/db.js
 const { Pool } = require('pg');
 require('dotenv').config();
-
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false, // importante pro Render
+    rejectUnauthorized: false,
   },
 });
 
