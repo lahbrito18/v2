9fbd89fe87e1d0326b806f5063219a529e2e59f3
lahbrito18 <134542731+lahbrito18@users.noreply.github.com>
2025-06-08 15:25:05 -0400

    Update db.js

--- banco/db.js
+++ banco/db.js
@@ -2,9 +2,17 @@ const { Pool } = require('pg');
 require('dotenv').config();
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+});
+
+// Teste de conexão
+pool.connect((err, client, release) => {
+  if (err) {
+    console.error('Erro ao conectar com PostgreSQL:', err.stack);
+  } else {
+    console.log('Conectado ao PostgreSQL no Render');
+    release();
+  }
 });
 
 module.exports = pool;
